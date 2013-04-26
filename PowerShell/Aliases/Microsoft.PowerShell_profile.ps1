1ad461187a5996859dfa2c266a2e81435c39ba99
Marslo <marslo.vida@gmail.com>
2013-04-26 20:26:34 +0800

    Add the alias file about Powershell

---
+++ PowerShell/Aliases/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,60 @@
+Set-Alias c Clear-Host
+Set-Alias l Get-ChildItem
+function la
+{
+    $currentpath = Get-Location
+    Get-ChildItem $currentpath -force
+}
+function tree
+{
+    $currentpath = Get-Location
+    Get-ChildItem $currentpath -recurse
+}
+function al
+{
+    Set-Location "C:\Marslo\Job\Summa\FE Tools\LotusNotes\AutoLotusNotes"
+}
+function ats
+{
+    Set-Location "C:\Marslo\Job\Summa\FE Tools\AutoScritps"
+}
+function desk
+{
+    Set-Location "C:\Users\jiaoolii\Desktop"
+}
+function prog
+{
+    Set-Location "C:\Program Files"
+}
+function gr
+{
+    Set-Location c:\
+}
+function gh
+{
+    Set-Location "C:\Users\jiaoolii"
+}
+function ex
+{
+    explorer .
+}
+function openit
+{
+    param(
+            $p
+        )
+    if(Test-Path $p){
+        # Write-Output $p
+        explorer.exe $p
+    } else{
+            "Could not find the $p"
+    }
+}
+function alias
+{
+   Get-Content "C:\Users\jiaoolii\Documents\WindowsPowerShell\aliases_Marslo.txt"
+}
+function wp
+{
+    Set-Location "C:\Users\jiaoolii\Documents\WindowsPowerShell"
+}
---
+++ PowerShell/README.md
@@ -0,0 +1,19 @@
+# Aliases File Location:
+Save the file into `%USERPROFILE%Documents`
+
+
+# Error
+
+## Error1:
+### Description:
+<pre><code>script1.ps1 cannot be loaded because running scripts is disabled on this system. For more information, see about_Execution_Policies at http://go.microsoft.com/fwlink/?LinkID=135170
+</code></pre>
+### Soluction
+<pre><code>set-executionpolicy remotesigned</code></pre>
+
+
+
+
+
+
+
